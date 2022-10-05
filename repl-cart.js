e0437d7f6381111e1dbf896216208b5d1380a20e
Greg Fenton <greg.fenton@gmail.com>
2022-10-05 12:35:08 -0400

    REPL implemented

--- repl-cart.js
+++ repl-cart.js
@@ -1,11 +1,14 @@
 import rl from 'readline-sync';
 
 let commands = ['list products'];
-console.log('The commands are:', commands);
-const theCommand = rl.question('What is your command? ');
 
-if (theCommand === 'list products') {
-  console.log('Here is the list of products: ....');
-} else {
-  console.log(`Invalid command: ${theCommand}`);
+while (true) {
+  console.log('The commands are:', commands);
+  const theCommand = rl.question('What is your command? ');
+  
+  if (theCommand === 'list products') {
+    console.log('Here is the list of products: ....');
+  } else {
+    console.log(`Invalid command: ${theCommand}`);
+  }
 }
